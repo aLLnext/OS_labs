0913be5faa96cf37cce833ecba8a0c3411d9545b
Anton Mikhailenko <toxaxab27@gmail.com>
2020-06-04 13:21:48 +1000

    Add lab8

--- Lab8/client.c
+++ Lab8/client.c
@@ -1,6 +1,3 @@
-//
-// Created by sapiest on 27.05.2020.
-//
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -9,57 +6,64 @@
 #include <string.h>
 #include <zconf.h>
 
-#define PORT 8080
-#define MAX 80
+#define PORT 9037
+#define MAXLINE 1023
 #define SA struct sockaddr
 
-
-void send_msg(int sockfd){
-  char buff[MAX];
-  int n;
-  while (1){
-    bzero(buff, sizeof(buff));
-    printf("Enter the string: ");
-    n = 0;
-    while ((buff[n++] = getchar())!= '\n');
-    write(sockfd, buff, sizeof(buff));
-    bzero(buff, sizeof(buff));
-    read(sockfd, buff, sizeof(buff));
-    printf("From server: %s", buff);
-    if((strncmp(buff, "exit", 4)) == 0){
-      printf("Client exit.\n");
-      break;
-    }
-
-  }
-}
-
-int main() {
-  //объявляем дескрипторы
-  int sockfd;
+int m2ain() {
+  char buff[MAXLINE + 1];
+  int sockfd, maxfd;
+  fd_set all_set, rset; //дескрипторы для select
   struct sockaddr_in servaddr, cli;
 
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-    fprintf(stderr, "Socket creation failed\n");
-
-    return EXIT_FAILURE;
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { //создаем сокет
+    printf("Socket creation failed...\n");
+    exit(EXIT_FAILURE);
   }
-
   bzero(&servaddr, sizeof(servaddr));
 
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   servaddr.sin_port = htons(PORT);
 
-  if(connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0){
-    fprintf(stderr, "Socket creation failed\n");
-
-    return EXIT_FAILURE;
+  if ((connect(sockfd, (SA *)&servaddr, sizeof(servaddr))) == -1) { //Устанавливает соединение с сервером.
+    printf("Connection with the server failed...\n");
+    exit(EXIT_FAILURE);
   }
 
-  send_msg(sockfd);
+  maxfd = sockfd + 1;
+  FD_ZERO(&all_set);
+  FD_SET(STDIN_FILENO, &all_set); //добавляем сокет на stdin
+  FD_SET(sockfd, &all_set); //добавляем сокет на сосединенеи с сервером
+  struct timeval tv; tv.tv_sec = 2; tv.tv_usec = 0;
 
+  puts("Enter message: ");
+  while (1){
+    rset = all_set;
+    select(maxfd, &rset, NULL, NULL, &tv); //используем для отслеживания нескольких сокетов
+
+    if(FD_ISSET(STDIN_FILENO, &rset)){ //если текущий сокет - stdin
+      bzero(buff, sizeof(buff));
+      int bytes = read(STDIN_FILENO, buff, MAXLINE); //считываем введенное пользователем
+      buff[bytes - 1] = '\0';
+      if(send(sockfd, buff, bytes + 1, 0) < 0)//отправляем на сервер
+      {
+        perror("Client send failed");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    if(FD_ISSET(sockfd, &rset)){
+      bzero(buff, sizeof(buff));
+      if(recv(sockfd, buff, MAXLINE + 1, 0) < 0){ //получаем данные с сервера
+        perror("Client recv failed");
+        exit(EXIT_FAILURE);
+      }
+      printf("From server: %s\n", buff);
+      //buff[0] = '\0';
+      puts("Enter message:");
+    }
+  }
   close(sockfd);
-
   return EXIT_SUCCESS;
 }
--- Lab8/server.c
+++ Lab8/server.c
@@ -1,115 +1,185 @@
-//
-// Created by sapiest on 27.05.2020.
-//
 
+#include <arpa/inet.h>
+#include <signal.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/types.h>
-#include <zconf.h>
+#include <time.h>
+#include <unistd.h>
 
-#define PORT 8080
+#define PORT 9037
 #define SA struct sockaddr
 #define MAXLINE 1024
 
-void wait_request(int sockfd) {
+int master_socket = 0; //основной сокет
+int is_running = 1;
+
+char * open_file(char* filename){
+  int fd;
+  char * buff;
+  struct stat st;
+  fd = open(filename, O_RDONLY);
+
+  stat(filename, &st);
+  buff = (char*)malloc(st.st_size * sizeof(char));
+  if(fd < 0){
+    perror("Can`t open file");
+    return "Error: Can`t open file";
+    //exit(EXIT_FAILURE);
+  }
+  read(fd, buff, st.st_size);
+
+  close(fd);
+
+  return buff;
+}
+
+int wait_request(int sockfd) {
   char buff[MAXLINE];
-  int n;
+  int bytes;
+  char m_time[] = "time";
+  char m_read[] = "open";
 
-  while (1) {
-    bzero(buff, sizeof(buff));
+  bzero(buff, sizeof(buff));
+  if ((bytes = recv(sockfd, buff, MAXLINE, 0)) > 0) {  //получаем данные от клиента
+    printf("From client: %s\n", buff);
 
-    read(sockfd, buff, sizeof(buff));
-    if(strncmp("time", buff, 4) == 0){
-      printf("Current time.\n");
-    }else {
-      printf("From client: %s\n To client : ", buff);
+    if (strcmp(m_time, buff) == 0) { //получаем текущее время
+      time_t now = time(0);
+      char *time_str = ctime(&now);
+      char *time_prefix = "Current time: ";
+      time_str[strlen(time_str) - 1] = '\0';
+      char *str = (char *)malloc(1 + strlen(time_str) + strlen(time_prefix));
+      strcpy(str, time_prefix);
+      strcpy(str, time_str);
+
+      if (send(sockfd, str, strlen(str), 0) == -1) { //отправляем данные клиенту
+        perror("Server send error");
+        // exit(EXIT_FAILURE);
+      }
+      free(str);
+      return bytes;
     }
-    bzero(buff, sizeof(buff));
 
-    n = 0;
-    while ((buff[n++] = getchar()) != '\n');
-    write(sockfd, buff, sizeof(buff));
+    if(strncmp(m_read, buff, 4) == 0 && strlen(buff) > 7){ //получаем команду open "filename", открываем файл
+      char * str = (char*)malloc(1 + strlen(buff) - 2);
+      strncpy(str, buff + 6, strlen(buff) - 7);
+      char * file_buff = open_file(str);
 
+      if (send(sockfd, file_buff, strlen(file_buff), 0) == -1) { //отправляем результат клиенту
+        perror("Server send error");
+        // exit(EXIT_FAILURE);
+      }
+      free(str);
+      return bytes;
+    }
 
-
-    if (strncmp("exit", buff, 4) == 0) {
-      printf("Server exit.\n");
-      break;
+    if (send(sockfd, buff, bytes, 0) == -1) { //отправляем результат клиенту
+      perror("Server send error");
     }
   }
+
+  if (bytes < 0) {
+    perror("Server recv error");
+  }
+
+  return bytes;
+}
+
+void interrupt_signal(int signal) {
+  is_running = 0;
+  printf("Interrupt received: Server exit.");
+  close(master_socket);
+  return;
 }
 
 int main() {
-  //дескрипторы и длина
-  int sockfd, connfd, updfd, nready, maxfdp1;
-  char buffer[MAXLINE];
-  pid_t childpid;
-  fd_set rset;
-  ssize_t n;
-  socklen_t len;
-  const int on = 1;
+  signal(SIGINT, interrupt_signal);
+
+  fd_set master; //основной дескриптор
+  fd_set allset; // дескриптор для select
+  int fdmax;     //текущий максимальный дескриптор
   struct sockaddr_in servaddr, cli;
+  socklen_t len;
+  int option = 1; //для setsockopt
 
+  int newfd;
 
-  //создаем сокет
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-    fprintf(stderr, "Socket creation failed\n");
+  FD_ZERO(&master); //обнуляем дескрипторы
+  FD_ZERO(&allset);
 
-    return EXIT_FAILURE;
+  if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) { //создаем сокет
+    perror("Socket creation failed\n");
+    exit(EXIT_FAILURE);
   }
-  printf("Socket successfully created..\n");
+  if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, &option, //ставим флаг, чтобы можно было переиспользовать порт
+                 sizeof(option)) != 0) {
+    perror("Socket setsockopt failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-  //обнуляем (n bytes = 0 в servaddr)
-  bzero(&servaddr, sizeof(servaddr));
-
-  // задаем IP и порт
+  bzero(&servaddr, sizeof(servaddr)); //сокет для работы с протоколами IP
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(PORT);
+  servaddr.sin_port = htons(PORT);     // Указываем порт сокета
 
-  if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0) {
-    fprintf(stderr, "Socket bind failed\n");
-
-    return EXIT_FAILURE;
+  if ((bind(master_socket, (SA *)&servaddr, sizeof(servaddr))) != 0) {  //Связывает сокет с конкретным адресом.
+    perror("Socket bind failed\n");
+    exit(EXIT_FAILURE);
   }
   printf("Socket successfully binded..\n");
 
-  if (listen(sockfd, 10) != 0) {
-    fprintf(stderr, "Listen failed\n");
-
-    return EXIT_FAILURE;
+  if (listen(master_socket, 10) != 0) {  //Подготавливает привязываемый сокет к принятию входящих соединений.
+    perror("Listen failed\n");
+    exit(EXIT_FAILURE);
   }
   printf("Server listening..\n");
 
-  FD_ZERO(&rset);
+  FD_SET(master_socket, &master); //добавляет master_socket в сет сокетов
 
-  while (1){
-    FD_SET(sockfd, &rset);
+  fdmax = master_socket; //устанавливаем крайний сокет
 
-    nready = select(sockfd +1, &rset, NULL, NULL, NULL);
-    if(FD_ISSET(sockfd, &rset)){
-      len = sizeof(cli);
+  while (is_running) {
+    allset = master; // копируем сет сокетов
+    if (select(fdmax + 1, &allset, NULL, NULL, NULL) == -1) { //используем для отслеживания нескольких сокетов
+      perror("Server select error\n");
+      exit(EXIT_FAILURE);
+    }
 
-      if ((connfd = accept(sockfd, (SA *)&cli, &len)) < 0) {
-        fprintf(stderr, "Server accept failed\n");
-
-        return EXIT_FAILURE;
-      }
-      printf("server acccept the client...\n");
-
-      if((childpid = fork()) == 0){
-        close(sockfd);
-        wait_request(connfd);
+    for (int i = 0; i < fdmax + 1; ++i) {
+      if (!FD_ISSET(i, &allset)) { //если сокета нет, то пропускаем
+        continue;
       }
 
-      close(connfd);
+      if (i == master_socket) {  //Поступил новый запрос на соединение
+        len = sizeof(cli);
+        if ((newfd = accept(master_socket, (SA *)&cli, &len)) < 0) { //Используется для принятия запроса на установление соединения от удаленного хоста.
+          perror("Server accept failed\n");
+          exit(EXIT_FAILURE);
+        }
+        FD_SET(newfd, &master); //добавляем новый сокет в сет
+        if (newfd > fdmax) {
+          fdmax = newfd;
+        }
+        printf("New connection , socket fd is %d , ip is : %s , port : %d\n",
+               newfd, inet_ntoa(cli.sin_addr), ntohs(cli.sin_port));
+      } else {
+        int response_code = wait_request(i); //
+        if (response_code > 0) {
+          continue;
+        }
+        if (response_code == 0) {
+          printf("Server: socket %d hung up\n", i);
+        }
+        close(i); // bye!
+        FD_CLR(i, &master);
+      }
     }
   }
-
-
-  return EXIT_SUCCESS;
 }
